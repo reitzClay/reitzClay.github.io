aeb55f3bad1a5a3f14d6cc669973f530e1bf4463
Clayton Reitz <clayton.reitz@outtask.nl>
2024-07-17 15:04:34 +0200

    added canvas and js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// Function draw method to draw 2d objects.
+
+function draw() {
+    const canvas = document.getElementById("tutorial");
+    if (canvas.getContext){
+        const ctx = canvas.getContext("2d");    
+    }
+}
+window.addEventListener("load", draw);
