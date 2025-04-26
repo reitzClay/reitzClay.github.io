209848db6ddbb341c7c453191555ee8c5b2cb86d
reitzClay <reitzclayton@gmail.com>
2025-04-26 21:07:56 +0200

    restart

--- js/index.js
+++
@@ -1,9 +0,0 @@
-// Function draw method to draw 2d objects.
-
-function draw() {
-    const canvas = document.getElementById("tutorial");
-    if (canvas.getContext){
-        const ctx = canvas.getContext("2d");    
-    }
-}
-window.addEventListener("load", draw);
